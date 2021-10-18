7e2fa99fd849e5f47887f892fa41ab2fa2cdd1c0
Rafa Puerta <info@rafapuerta.com>
2021-10-18 20:26:25 +0200

    4 - Metodos

--- 4-Metodos/ej1/index.js
+++ 4-Metodos/ej1/index.js
@@ -4,9 +4,50 @@ let app = express();
 let personas = require("./personas");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/personas", (req, res) => {
   res.send(personas);
+});
+
+app.post("/anyadir", (req, res) => {
+  console.log(req.body);
+  let persona = req.body.persona;
+  personas.push(persona);
+  console.log(personas);
+  res.send({ mensaje: "todo correcto" });
+});
+
+app.put("/modificar", (req, res) => {
+  let found = false;
+  for (let i = 0; i < personas.length; i++) {
+    if (
+      req.body.persona.nombre.toLowerCase() === personas[i].nombre.toLowerCase()
+    ) {
+      personas[i].apellido = req.body.persona.apellido;
+      personas[i].edad = req.body.persona.edad;
+      console.log(personas);
+      found = true;
+    }
+  }
+  found
+    ? res.send({ mensaje: "Persona modificada" })
+    : res.send({ mensaje: "No se ha encontrado" });
+});
+
+app.delete("/borrar", (req, res) => {
+  let found = false;
+  for (let i = 0; i < personas.length; i++) {
+    if (req.body.nombre.toLowerCase() === personas[i].nombre.toLowerCase()) {
+      personas.splice(i, 1);
+      console.log(personas);
+      found = true;
+    }
+  }
+  found
+    ? res.send({ mensaje: "Persona borrada" })
+    : res.send({ mensaje: "No se ha encontrado" });
 });
 
 app.listen(process.env.PORT || 3000);
--- 4-Metodos/ej1/public/indexFront.js
+++ 4-Metodos/ej1/public/indexFront.js
@@ -7,3 +7,19 @@ fetch("/personas")
       ).innerHTML += `<div><h3>${data[i].nombre}</h3><h4>${data[i].apellido}</h4><p>${data[i].edad}</p></div>`;
     }
   });
+
+function borrar() {
+  fetch("/borrar", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre: "paquita" }),
+  })
+    .then((res) => res.json())
+    .then(function (datos) {
+      document.getElementById(
+        "feedback"
+      ).innerHTML = `<h3>${datos.mensaje}</h3>`;
+    });
+}
